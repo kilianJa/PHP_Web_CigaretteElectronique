587d3a274f9ccadcb846e1e8882ce6b7db6f3c1c
Anthony_Ubun <darkword62149@gmailcom>
2020-11-25 10:38:19 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>E-Cigarette</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+        <link href="./css/style.css" rel="stylesheet">
+        <link href="./css/header.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php include_once('./lib/header.php') ?>
+    </body>
+    <script>
+        setTimeout(function () { location.reload(1); }, 10000);
+    </script>
+</html>
---
+++ lib/header.php
@@ -0,0 +1,34 @@
+<nav id="nav-bar" class="fixed vcenter f r jcsb">
+    <div class="f r left">
+        <i class='fas fa-smoking'></i>
+        <div>
+            <h3>E-Cigarette</h3>
+            <lore>La cigarette elctronique BIO</lore>
+        </div>
+    </div>
+
+    <div class="f r tac center">
+        <i class='fas fa-search'></i>
+        <input type="text" placeholder="Rechercher" />
+    </div>
+    
+
+    <ul class="f r">
+        <li>
+            <i class='ccou 	fas fa-user-astronaut'></i>
+            <lore>Mon compte</lore>
+        </li>
+        <li>
+            <i class='far fa-question-circle'></i>
+            <lore>Aide</lore>
+        </li>
+        <li>
+            <i class='far fa-heart'></i>
+            <lore>Mes envies</lore>
+        </li>
+        <li>
+            <i class='fas fa-shopping-basket'></i>
+            <lore>Mon panier</lore>
+        </li>
+    </ul>
+</nav>
